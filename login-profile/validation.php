60e7ee43c7a1efc613facbcd2bfd281956c1815c
Rajdeep Sengupta <senguptarajdeep21@gmail.com>
2021-03-29 14:09:13 +0530

    initial commit

---
+++ login-profile/validation.php
@@ -0,0 +1,21 @@
+<?php
+session_start();
+
+$con = mysqli_connect('localhost','root','');
+mysqli_select_db($con,'userprofiles');
+$email = $_POST['email'];
+$pass = $_POST['password'];
+$s = "select * from profiles where email='$email' && password='$pass'";
+$result = mysqli_query($con,$s);
+$num = mysqli_num_rows($result);
+if($num==1){
+    $row = mysqli_fetch_array($result);
+    $_SESSION['name']=$row['name'];
+    $_SESSION['logged_in']=true;
+    header('location:profile.php');
+}
+else{
+echo "<h3 style='font-family:Segoe UI;'>Login Error</h3>";
+echo "<script>setTimeout(\"location.href = 'login.html';\",1500);</script>";
+}
+?>
